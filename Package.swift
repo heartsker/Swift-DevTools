1fb6ea373017482ff83260e5c056dac0e1e32f15
heartsker <heartsker@gmail.com>
2022-11-19 13:10:17 +0300

    Adds Package.swift

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "DevTools",
+    platforms: [.iOS("16.0")],
+    products: [
+        .library(
+            name: "DevTools",
+            targets: ["DevTools"])
+    ],
+    dependencies: [
+        // Base package
+    ],
+    targets: [
+        .target(
+            name: "DevTools",
+            dependencies: []),
+        .testTarget(
+            name: "DevToolsTests",
+            dependencies: ["DevTools"])
+    ]
+)
